08c57667714303a5c27f21c0ad3d53a53de7a906
kavya10100 <kavyaganatra13@gmail.com>
2025-02-03 17:44:56 -0500

    ice

--- ice/client/app/list.js
+++ ice/client/app/list.js
@@ -1,14 +1,34 @@
-/*
- *  Name: Kavya Ganatra
-    File: list.js
- */
+import animalService from "./animal.service.mock.js";
+
 console.log('we are on the list page');
+
+//http://127.0.0.1:5501/ice/client/list.html?page=2&perPage=15
+const params = new URL(document.location).searchParams;
+//add records for pagination test
+let recCount = params.get("records");
+if(recCount !== null){
+    let index = 0;
+    while(recCount-->0) {
+        animalService.saveAnimal({
+            "name": `name ${index++}`,
+            "breed": "Grizzly Bear",
+            "legs": 4,
+            "eyes": 2,
+            "sound": "Moo"
+          });
+    }    
+}
 
 /* do table stuff */
 const eleEmpty = document.getElementById('empty-message');
 const eleTable = document.getElementById('animal-list');
 
-const records = animalService.getAnimals();
+//const records = animalService.getAnimals();
+let recordPage = {
+    page: Number(params.get('page') ?? 1),
+    perPage: Number(params.get('perPage') ?? 7)
+}
+const {records, pagination} = animalService.getAnimalPage(recordPage);
 
 if (!records.length) {
     eleEmpty.classList.remove('d-none');
@@ -17,6 +37,32 @@ if (!records.length) {
     eleEmpty.classList.add('d-none');
     eleTable.classList.remove('d-none');
     drawAnimalTable(records);
+    drawPagination(pagination);
+}
+/* 
+ * 
+ */
+function drawPagination({ page = 1, perPage = 5, pages = 10 }) 
+{
+    const pagination = document.getElementById('pagination');
+    if (pages > 1) { 
+        pagination.classList.remove('d-none');
+    }
+    const ul = document.createElement("ul");
+    ul.classList.add('pagination')
+    ul.insertAdjacentHTML('beforeend', addPage(page-1, 'Previous', (page == 1) ? 'disabled' : ''))
+    for (let i = 1; i <= pages; i++) {
+        ul.insertAdjacentHTML('beforeend', addPage(i, i, (i == page) ? 'active' : ''));
+    }
+    ul.insertAdjacentHTML('beforeend', addPage(page+1, 'Next', (page == pages) ? 'disabled' : ''))
+
+    pagination.append(ul);
+
+    function addPage(number, text, style) {
+      return `<li class="page-item ${style}">
+        <a class="page-link" href="./list.html?page=${number}&perPage=${perPage}">${text}</a>
+      </li>`
+    }
 }
 /* 
  * 
