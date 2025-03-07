fb0046328109637081f423cef763cbcb7668549b
kavya10100 <kavyaganatra13@gmail.com>
2025-03-07 14:00:35 -0500

    ice

--- ice/client/app/animals/index.js
+++ ice/client/app/animals/index.js
@@ -27,15 +27,15 @@ async function animal(name) {
         const mb3Leg = document.createElement('div');
         mb3Leg.classList.add('mb-3');
         mb3Leg.innerHTML = '<label for="legs" class="form-label">Number of Legs</label>' +
-            `<input type="text" class="form-control" id="legs" name="legs" value="${animal!=null?animal.legs:""}">` +
-            
+        `<input type="text" class="form-control" id="legs" name="legs" value="${animal!=null?animal.legs:""}">` +
+
             '<p class="text-danger d-none"></p>';
         container.append(mb3Leg);
         
         const mb3Eye = document.createElement('div');
         mb3Eye.classList.add('mb-3');
         mb3Eye.innerHTML = '<label for="eyes" class="form-label">Number of Eyes</label>' +
-            `<input type="text" class="form-control" id="eyes" name="eyes" value="${animal!=null?animal.eyes:""}">` +
+        `<input type="text" class="form-control" id="eyes" name="eyes" value="${animal!=null?animal.eyes:""}">` +
 
             '<p class="text-danger d-none"></p>';
         container.append(mb3Eye);
@@ -43,9 +43,8 @@ async function animal(name) {
         const mb3Sound = document.createElement('div');
         mb3Sound.classList.add('mb-3');
         mb3Sound.innerHTML = '<label for="sound" class="form-label">Sound this animal makes</label>' +
-            `<input type="text" class="form-control" id="sound" name="sound" value="${animal!=null?animal.sound:""}">` +
+        `<input type="text" class="form-control" id="sound" name="sound" value="${animal!=null?animal.sound:""}">` +
 
-            
             '<p class="text-danger d-none"></p>';
         container.append(mb3Sound);        
 
